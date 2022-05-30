a553d7523daf5a44ecc4841adc6c9008d650d892
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-30 18:50:57 +0200

    images

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ io.on("connection", socket => {
         interval = setInterval(() => {
             game.update()
             io.emit("gameState", game)
-        }, 5)
+        }, 3)
     }
 
     if (io.sockets.sockets.size === 1) gameInterval()
