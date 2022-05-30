118cd131d28b0ede57f1c7d2f428d789569c9f81
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-30 18:40:30 +0200

    changing speed

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ io.on("connection", socket => {
         interval = setInterval(() => {
             game.update()
             io.emit("gameState", game)
-        })
+        }, 5)
     }
 
     if (io.sockets.sockets.size === 1) gameInterval()
